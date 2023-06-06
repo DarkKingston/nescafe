4fa669140e04fad832df6d6273d1ce117eb364ef
webmaster_prog <dmitriis.webmaster@gmail.com>
2023-06-06 10:49:35 +0300

    js

--- web/themes/custom/nescafe/js/script.js
+++ web/themes/custom/nescafe/js/script.js
@@ -1,15 +1,7 @@
+function toggleMobMenu(){
+  $('.mobile_menu').slideToggle();
+}
 
-(function ($) {
-  Drupal.behaviors.navbarBehavior = {
-    attach: function (context, settings) {
-      function toggleMobMenu(){
-        $('.mobile_menu').slideToggle();
-      }
-
-      function toggleLangs() {
-        $('.current_lang').toggleClass('active');
-      }
-    }
-  };
-
-})(jQuery);
+function toggleLangs() {
+  $('.current_lang').toggleClass('active');
+}
