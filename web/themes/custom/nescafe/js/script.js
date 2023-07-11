c042096be0ab6ff1304b73407e8290470831dab8
= <=>
2023-07-11 15:20:56 +0300

    theme

--- web/themes/custom/nescafe/js/script.js
+++ web/themes/custom/nescafe/js/script.js
@@ -39,10 +39,15 @@
 
       document.querySelector('.form-item-field-reglament-value input').disabled = true;
 
-        document.querySelector('.reg_btn').href='/reglament';
       if(document.querySelector(".reglament_accept")){
         document.querySelector('.form-item-field-reglament-value input').checked = true;
         document.querySelector('.reg_btn').href='/user/register';
+      }else{
+        document.querySelector('.reg_btn').addEventListener("click", () => {
+          window.location.replace(
+            '/reglament',
+          );
+        });
       }
 
     }
